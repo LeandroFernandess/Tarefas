897c71610a120033258b959bb15312f1ae952465
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-08-07 22:29:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
 import streamlit as st
-from Functions.Function import AddTask, LoadTask, DisplayTask, Auth, SetupFirebase
-
+from Functions.Function import AddTask, LoadTask, DisplayTask
+import firebase_admin
+from firebase_admin import credentials
+import os
+import json
 
 # Configurações iniciais do Streamlit
 st.set_page_config(page_title="Tarefas", page_icon="📝")
 
+# Função de autenticação
+def authenticate(password):
+    if st.session_state.get("authenticated", False):
+        return True
+
+    # Obtenha a senha da variável de ambiente e log para depuração
+    app_password = os.getenv("APP_PASSWORD")
+    st.write(f"DEBUG: Stored password: {app_password}")  # Log de depuração
+
+    if password == app_password:
+        st.session_state.authenticated = True
+        return True
+
+    return False
+
+# Configurar o Firebase
+def setup_firebase():
+    if not firebase_admin._apps:
+        # Obtém as credenciais do Firebase das variáveis de ambiente
+        credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        cred = credentials.Certificate(json.loads(credentials_json))
+        # Obtém a databaseURL das variáveis de ambiente
+        database_url = os.getenv("DATABASE_URL")
+        firebase_admin.initialize_app(
+            cred,
+            {"databaseURL": database_url},
+        )
 
 # Função principal para inserção e exibição de tarefas
-def App():
+def main_app():
     st.sidebar.header("Desenvolvido por :red[Leandro Fernandes]", divider="gray")
-
+    
     # Sidebar com opções de categoria
     page = st.sidebar.radio(
         "Selecione a página",
@@ -56,9 +86,8 @@ def App():
         st.write("---")
         DisplayTask(user_id, task_list, page)
 
-
 if __name__ == "__main__":
-    SetupFirebase()
+    setup_firebase()
 
     if "authenticated" not in st.session_state:
         st.session_state.authenticated = False
@@ -70,10 +99,10 @@ if __name__ == "__main__":
         )
         password = st.text_input("Insira a senha:", type="password")
         if st.button("Entrar"):
-            if Auth(password):
+            if authenticate(password):
                 st.success("Autenticado com sucesso!")
-                st.rerun()  # Reexecuta a aplicação após autenticação bem-sucedida
+                st.experimental_rerun()  # Reexecuta a aplicação após autenticação bem-sucedida
             else:
                 st.error("Senha incorreta. Tente novamente.")
     else:
-        App()
+        main_app()
