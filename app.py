864d2b5059cf3fe9d0e8f1052ece381fc3ff7586
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-08-07 21:53:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials
-from Functions.Function import AddTask, LoadTask, DisplayTask
-
-# Configuração do Firebase Admin SDK
-if not firebase_admin._apps:
-    cred = credentials.Certificate("credentials.json")  # Atualize esse caminho
-    firebase_admin.initialize_app(
-        cred,
-        {
-            "databaseURL": "https://tarefas-9493e-default-rtdb.firebaseio.com"  # Atualize com o URL do seu banco de dados
-        },
-    )
-
-st.set_page_config(page_title="Tarefas")
-
-# Sidebar com opções de categoria
-page = st.sidebar.radio(
-    "Selecione a página", ["Inserir tarefa", "Tarefas diárias", "Tarefas gerais"]
+from Functions.Function import (
+    AddTask,
+    LoadTask,
+    DisplayTask,
+    SetupFirebase,
+    Authenticate,
 )
+
+# Configurações iniciais do Streamlit
+st.set_page_config(page_title="Tarefas", page_icon="📝")
 
 
 # Função principal para inserção e exibição de tarefas
-def Main():
+def App():
+    st.sidebar.header("Desenvolvido por :red[Leandro Fernandes]", divider="gray")
+
+    # Sidebar com opções de categoria
+    page = st.sidebar.radio(
+        "Selecione a página",
+        ["Inserir tarefa", "Tarefas diárias", "Tarefas gerais"],
+    )
+    st.sidebar.divider()
+    st.sidebar.text(
+        """
+        Sistema desenvolvido para realizar\nanotações do trabalho e demais\natividades.📖
+        """
+    )
+
     # Defina um ID de usuário fixo, ou modifique conforme necessário
     user_id = "default_user_id"
 
@@ -32,6 +36,7 @@ def Main():
             "<h1 style='text-align: center;'>Adicionar Nova Tarefa</h1>",
             unsafe_allow_html=True,
         )
+        st.write("---")
         new_task = st.text_area("Insira uma nova tarefa aqui:")
         task_category = st.selectbox(
             "Selecione a categoria da tarefa", ["Tarefas diárias", "Tarefas gerais"]
@@ -43,17 +48,37 @@ def Main():
             else:
                 st.success("Tarefa inserida com sucesso")
                 AddTask(user_id, new_task, task_category)
+                new_task = ""
 
     else:
         # Carregar e exibir tarefas com base na categoria selecionada na sidebar
         task_list = LoadTask(user_id) or {}
 
         st.markdown(
-            f"<h3 style='text-align: center;'>Tarefas: {page}</h3>",
+            f"<h2 style='text-align: center;'>{page}</h2>",
             unsafe_allow_html=True,
         )
+        st.write("---")
         DisplayTask(user_id, task_list, page)
 
 
 if __name__ == "__main__":
-    Main()
+    SetupFirebase()
+
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+
+    if not st.session_state.authenticated:
+        st.markdown(
+            "<h1 style='text-align: center;'>Gerenciamento de tarefas - Login</h1>",
+            unsafe_allow_html=True,
+        )
+        password = st.text_input("Insira a senha:", type="password")
+        if st.button("Entrar"):
+            if Authenticate(password):
+                st.success("Autenticado com sucesso!")
+                st.rerun()
+            else:
+                st.error("Senha incorreta. Tente novamente.")
+    else:
+        App()
